591e313366c8b1244d3a647d2792967b2ba0b5d4
Hristijan <hristeski2@gmail.com>
2025-03-31 00:25:30 +0200

    novo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, importProvidersFrom } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { EventListComponent } from './components/event-list/event-list.component';
+import { AddEventComponent } from './components/add-event/add-event.component';
+import { EventDetailComponent } from './components/event-detail/event-detail.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+
 @NgModule({
-  declarations: [AppComponent],  // Removed standalone components
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    EventListComponent,
+    AddEventComponent,
+    EventDetailComponent,
+    SearchBarComponent
+  ],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+    BrowserModule,        // includes CommonModule
+    ReactiveFormsModule,  // for reactive forms directives like formGroup
+    HttpClientModule,     // if using HttpClient in your services
     AppRoutingModule
   ],
-  providers: [
-    importProvidersFrom(RouterModule) // For standalone components
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
